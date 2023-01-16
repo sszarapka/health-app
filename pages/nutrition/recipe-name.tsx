e1847cc748c1b1ad2b71d4e5725d937fdd578268
Stanislaw <s.szarapka10@gmail.com>
2023-01-16 23:43:45 +0100

    redirect pages with server.js file

---
+++ pages/nutrition/recipe-name.tsx
@@ -0,0 +1,69 @@
+import { Typography, Image } from 'antd'
+const { Title, Text, Paragraph } = Typography
+
+interface RecipeProps {
+  name: string
+  type: 'Śniadanie' | 'Obiad' | 'Kolacja'
+  ingredients: string[]
+  description: string
+  macro: {
+    carbs: number
+    protein: number
+    fat: number
+  }
+  image: string
+}
+
+const mockedRecipe: RecipeProps = {
+  name: 'Naleśniory',
+  type: 'Śniadanie',
+  ingredients: [
+    '30g proszku do pieczenia',
+    '100g mąki',
+    '450g dobrego humoru',
+    'pajda chleba',
+  ],
+  description:
+    'lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem lore lorem ipsum dolor sit amet lorem',
+  macro: {
+    carbs: 56,
+    protein: 34,
+    fat: 19,
+  },
+  image: 'https://tygodnik.pl/news_foto/2018/0817/10127.jpg',
+}
+
+const Recipe = ({ name, type, ingredients, description }: RecipeProps) => {
+  const recipeIngredients = mockedRecipe.ingredients.map((ingredient, i) => (
+    <li key={i} className="ingredients__item">
+      {i + 1}. {ingredient}
+    </li>
+  ))
+  return (
+    <section className="recipe">
+      <Title level={2}>{mockedRecipe.name}</Title>
+      <Image
+        src={mockedRecipe.image}
+        alt="obraz"
+        className="recipe__image"
+        preview={false}
+      />
+      <div className="recipe__macro">
+        <Text>W: {mockedRecipe.macro.carbs}g</Text>
+        <Text>B: {mockedRecipe.macro.protein}g</Text>
+        <Text>T: {mockedRecipe.macro.fat}g</Text>
+      </div>
+      <Title level={4}>Składniki</Title>
+
+      <ul className="recipe__ingredients">{recipeIngredients}</ul>
+      <Title level={4}>Wykonanie</Title>
+
+      <Paragraph className="recipe__description">
+        {mockedRecipe.description}
+      </Paragraph>
+      <Text className="recipe__enjoy">Smacznego!</Text>
+    </section>
+  )
+}
+
+export default Recipe
